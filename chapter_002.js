f9608cd2b7ed4c066f974f745050b744a035fff4
Jose Luis Martinez <askme@email.com>
2018-04-14 17:25:01 +0200

    Delete Chess Board

--- chapter_002.js
+++ chapter_002.js
@@ -45,22 +45,3 @@ for (let n = 1; n<=100; n++) {
                             // String (its length is zero); otherwise the result
                             // is true
 }
-
-//Chess Board
-
-var size = 4;
-var output = "";
-
-for (let y = 0; y < size; y++) {
-  for (let x = 0; x < size; x++) {
-    if ((x + y) % 2 == 0) {
-     output +="#"
-    }
-    else {
-      output += " "
-     }
-   }
-   output += "\n"
- }
-
- console.log(output);
